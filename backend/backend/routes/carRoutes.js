01ea48cb3ec63fa20bb3ca9738f36aa598f82f69
zand4u <zand@me.com>
2024-08-30 09:19:17 +0200

    Ensure carRoutes.js is included

--- backend/backend/routes/carRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const carController = require('../controllers/carController');
-
-// Route för att lägga till en ny bil
-router.post('/', carController.addCar);
-
-module.exports = router;
